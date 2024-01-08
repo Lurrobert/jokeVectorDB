50ddde2c5bf4c58a7f0daf0e2ceede1221937884
Robert Lukoshka <robert.lukoshko@gmail.com>
2024-01-08 13:29:56 +0100

    first commit

---
+++ jokeDB.py
@@ -0,0 +1,11 @@
+impor numpy
+
+class VectorDB:
+    def __init__(self):
+        self.vector_db = []
+
+    def add_vector(self, vector):
+        self.vector_db.append(vector)
+
+    def get_vector(self, index):
+        return self.vector_db[index]
